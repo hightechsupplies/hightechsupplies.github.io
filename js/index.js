68d03028fe95ca97fc5426c5d613ecc3affc202a
hightechsupplies <hightech@webname.com>
2015-08-06 13:34:56 -0430

    mejora en products
    ta mas wonito

--- js/index.js
+++ js/index.js
@@ -7,37 +7,37 @@ function preload() {
 }
 
 preload(
-    "../imagenes/mapas/worldmap.jpg",
-    "../imagenes/mapas/northamerica.jpg",
-    "../imagenes/mapas/centralamerica.jpg",
-    "../imagenes/mapas/southamerica.jpg"
+    "imagenes/mapas/worldmap.jpg",
+    "imagenes/mapas/northamerica.jpg",
+    "imagenes/mapas/centralamerica.jpg",
+    "imagenes/mapas/southamerica.jpg"
 )
 
 $(document).ready(function(){
     $(".north").mouseenter(function(){
-        $("#map").attr("src","../imagenes/mapas/northamerica.jpg");
+        $("#map").attr("src","imagenes/mapas/northamerica.jpg");
         $("a.north").addClass( "hovered" );
     });
     $(".north").mouseleave(function(){
-        $("#map").attr("src","../imagenes/mapas/worldmap.jpg");
+        $("#map").attr("src","imagenes/mapas/worldmap.jpg");
         $("a.north").removeClass( "hovered" );
     });
     
     $(".central").mouseenter(function(){
-        $("#map").attr("src","../imagenes/mapas/centralamerica.jpg");
+        $("#map").attr("src","imagenes/mapas/centralamerica.jpg");
         $("a.central").addClass( "hovered" );
     });
     $(".central").mouseleave(function(){
-        $("#map").attr("src","../imagenes/mapas/worldmap.jpg");
+        $("#map").attr("src","imagenes/mapas/worldmap.jpg");
         $("a.central").removeClass( "hovered" );
     });
     
     $(".south").mouseenter(function(){
-        $("#map").attr("src","../imagenes/mapas/southamerica.jpg");
+        $("#map").attr("src","imagenes/mapas/southamerica.jpg");
         $("a.south").addClass( "hovered" );
     });
     $(".south").mouseleave(function(){
-        $("#map").attr("src","../imagenes/mapas/worldmap.jpg");
+        $("#map").attr("src","imagenes/mapas/worldmap.jpg");
         $("a.south").removeClass( "hovered" );
     });
 });
